13fd37bca4b01b7716d811b9f4a5e243a7ed1e39
androsrj <androsrj@gmail.com>
2024-07-09 16:53:45 -0500

    new variograms

--- initial_flood_figs.R
+++ initial_flood_figs.R
@@ -1,22 +1,30 @@
 library(MBA)
 library(fields)
+library(geoR)
 load("data/flood_data.RData")
 
 # Global covariates (Z)
+covars <- c(3, 4)
 STrain <- 10
 STest <- 5
+set.seed(123)
 samp <- sample(1:nrow(inputs), STrain + STest)
 stormsTrain <- samp[1:STrain]
 stormsTest <- samp[(STrain+1):(STrain+STest)]
-Z <- inputs[stormsTrain,3:4]
-ZTest <- inputs[stormsTest,3:4]
+Z <- inputs[stormsTrain, covars]
+ZTest <- inputs[stormsTest, covars]
 
 # Local covariates (X)
 n <- 500
 nTest <- 20
 samp2 <- sample(1:nrow(coords))
-train <- samp2[1:n]
-test <- samp2[(n+1):(n+nTest)]
+#train <- samp2[1:n]
+#train <- which(coords$x > -74.85 & coords$x < -74.83 & coords$y > 39.1 & coords$y < 39.125)
+train <- which(coords$x > -74.85 & coords$x < -74.83 & coords$y > 39.075 & coords$y < 39.1)
+n <- length(train)
+length(train)
+#test <- samp2[(n+1):(n+nTest)]
+test <- sample(train, nTest)
 X <- matrix(c(rep(1, n), coords$elev_meters[train]), ncol=2)
 XTest <- matrix(c(rep(1, nTest), coords$elev_meters[test]), ncol=2)
 
@@ -30,14 +38,21 @@ DTest <- rdist(UTest)
 Y <- matrix(as.matrix(out[stormsTrain, train]), ncol = 1)
 YTest <- matrix(as.matrix(out[stormsTest, test]), ncol = 1)
 
-# Storm 1
-pred.surf <-  mba.surf(cbind(UTest, YTest[1:nTest]), no.X=100, no.Y=100, extend=T)$xyz.est
-image.plot(pred.surf, xaxs ="r", yaxs = "r", main="Storm 1", 
-           cex.main = 1.5, col = hcl.colors(12, "YlOrRd", rev=TRUE))
-contour(pred.surf, add=T)
+par(mfrow=c(1,2))
+# Variogram (locations)
+full_U <- as.matrix(U) %x% matrix(1, nrow = STrain, ncol = 1)
+vg_loc <- variog(coords = full_U, data = Y)
+vg_model <- lm(vg_loc$v ~ vg_loc$u)
+plot(vg_loc, main = "Locations")
+abline(a=coef(vg_model)[1], b=coef(vg_model)[2], 
+       col = "brown1", lwd=2, lty=2)
 
-# Storm 2
-pred.surf <-  mba.surf(cbind(UTest, YTest[(nTest+1):(2*nTest)]), no.X=100, no.Y=100, extend=T)$xyz.est
-image.plot(pred.surf, xaxs ="r", yaxs = "r", main="Storm 2", 
-           cex.main = 1.5, col = hcl.colors(12, "YlOrRd", rev=TRUE))
-contour(pred.surf, add=T)
+# Variogram (global covariates)
+full_Z <- as.matrix(Z) %x% matrix(1, nrow = n, ncol = 1)
+vg_global <- variog(coords = full_Z, data = Y)
+vg_model2 <- lm(vg_global$v ~ vg_global$u)
+plot(vg_global, main = "Global Covariates")
+abline(a=coef(vg_model2)[1], b=coef(vg_model2)[2], 
+       col = "brown1", lwd=2, lty=2)
+
+
