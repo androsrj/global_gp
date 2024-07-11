bc9ac7b39020f2d9751841d81ccd347ef0078dcb
Jacob Andros <androsrj@gmail.com>
2024-07-11 15:44:18 -0500

    new flood plots

--- model_flood.sh
+++ model_flood.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
-#SBATCH --time=0-23:00:00
+#SBATCH --time=0-16:00:00
 #SBATCH --partition=medium
-#SBATCH --mem-per-cpu=64GB
+#SBATCH --mem-per-cpu=32GB
 #SBATCH --output=outfile_flood
 module purge
 module load R
