d6f439ceebf430e5bad3ae4e43bcfed35c147156
androsrj <androsrj@gmail.com>
2025-06-11 11:01:22 -0500

    simulations in parallel

--- run_sims.R
+++ run_sims.R
@@ -8,8 +8,11 @@ source("other_functions/helper_functions.R") # Other misc functions (not part of
 source("other_functions/bsplines_2_3D.R")
 library(MBA)
 library(fields)
+library(parallel) 
+library(doParallel)
+library(foreach) 
 
-run.sims <- function(scen, nReps, starting, propSD) {
+run.sims <- function(scen) {
   dir <- paste0("data/", size, "/scen", scen, "/")
   load(paste0(dir, "train.RData"))
   load(paste0(dir, "test.RData"))
@@ -26,6 +29,29 @@ run.sims <- function(scen, nReps, starting, propSD) {
   UTest <- test$U
   DTest <- test$D
   K <- 9
+  
+  propSD <- readRDS("objects/propSD.RDS")[[scen]]
+  starting <- list(sigma2 = runif(K, 50, 100),
+                   theta = rep(.25, K),
+                   sigf2 = 7,
+                   thf = 2, 
+                   tau2 = 0.3,
+                   beta = c(0, 0, 0))
+  if (scen == 2 | scen == 8) {
+    starting$thf <- 8
+  }
+  if (scen == 3 | scen == 9) {
+    starting$sigf2 <- 16
+  }
+  if (scen == 4) {
+    starting$tau2 <- 1.6
+  }
+  if (scen == 5 | scen == 10) {
+    starting$sigma2 <- runif(K, 5, 10)
+  } 
+  if (scen == 6 | scen == 11) {
+    starting$theta <- rep(2.5, K)
+  }
   
   results <- vector("list", length = nReps)
   for (i in 1:nReps) {
@@ -47,332 +73,26 @@ run.sims <- function(scen, nReps, starting, propSD) {
   beta <- apply(sapply(1:nReps, \(i) results[[i]]$posteriorMeans$beta), 1, mean)
   tau2 <- mean(sapply(1:nReps, \(i) results[[i]]$posteriorMeans$tau2))
   
-  return(list(acceptance = acc,
-              sigma2 = sigma2, 
-              theta = theta,
-              sigf2 = sigf2,
-              thetaf = thetaf,
-              beta = beta,
-              tau2 = tau2))
+  #cat(paste0("Scenario ", scen, ":\n"))
+  #cat("Avg. Acceptance: \n")
+  #acc
+  #cat("Sigma2: \n")
+  #sigma2
+  #cat("Theta: \n")
+  #theta
+  #cat("Sigma2_f: \n")
+  #sigf2
+  #cat("Theta_f: \n")
+  #thetaf
+  #cat("Beta: \n")
+  #beta
+  #cat("Tau2: \n")
+  #tau2
 }
 
-##### SCENARIO 1 #####
-propSD <- list(sigf2 = 0.4,
-               thf = 2,
-               sigma2 = seq(0.1, 0.2, length = K),
-               tau2 = 0.4,
-               theta = seq(0.4, 0.7, length = K))
-starting <- list(sigma2 = seq(50, 100, length = K),
-                 theta = rep(0.5, K),
-                 sigf2 = 6,
-                 thf = 1.3, 
-                 tau2 = 0.1,
-                 beta = c(0, 0, 0))
-scen1 <- run.sims(1, 10, starting, propSD)
-cat("Scenario 1: \n")
-cat("Avg. Acceptance: \n")
-scen1$acceptance
-cat("Sigma2: \n")
-scen1$sigma2
-cat("Theta: \n")
-scen1$theta
-cat("Sigma2_f: \n")
-scen1$sigf2
-cat("Theta_f: \n")
-scen1$thetaf
-cat("Beta: \n")
-scen1$beta
-cat("Tau2: \n")
-scen1$tau2
-
-##### SCENARIO 2 #####
-propSD <- list(sigf2 = 0.6,
-               thf = 5,
-               sigma2 = seq(0.1, 0.25, length = K),
-               tau2 = 0.4,
-               theta = seq(0.5, 0.8, length = K))
-starting <- list(sigma2 = seq(50, 100, length = K),
-                 theta = rep(0.5, K),
-                 sigf2 = 6,
-                 thf = 8, 
-                 tau2 = 0.1,
-                 beta = c(0, 0, 0))
-scen2 <- run.sims(2, 10, starting, propSD)
-cat("Scenario 2: \n")
-cat("Avg. Acceptance: \n")
-scen2$acceptance
-cat("Sigma2: \n")
-scen2$sigma2
-cat("Theta: \n")
-scen2$theta
-cat("Sigma2_f: \n")
-scen2$sigf2
-cat("Theta_f: \n")
-scen2$thetaf
-cat("Beta: \n")
-scen2$beta
-cat("Tau2: \n")
-scen2$tau2
-
-##### SCENARIO 3 #####
-propSD <- list(sigf2 = 0.6,
-               thf = 2,
-               sigma2 = seq(0.1, 0.25, length = K),
-               tau2 = 0.3,
-               theta = seq(0.5, 0.8, length = K))
-starting <- list(sigma2 = seq(50, 100, length = K),
-                 theta = rep(0.5, K),
-                 sigf2 = 15,
-                 thf = 1.5, 
-                 tau2 = 0.1,
-                 beta = c(0, 0, 0))
-scen3 <- run.sims(3, 10, starting, propSD)
-cat("Scenario 3: \n")
-cat("Avg. Acceptance: \n")
-scen3$acceptance
-cat("Sigma2: \n")
-scen3$sigma2
-cat("Theta: \n")
-scen3$theta
-cat("Sigma2_f: \n")
-scen3$sigf2
-cat("Theta_f: \n")
-scen3$thetaf
-cat("Beta: \n")
-scen3$beta
-cat("Tau2: \n")
-scen3$tau2
-
-##### SCENARIO 4 #####
-propSD <- list(sigf2 = 0.6,
-               thf = 2,
-               sigma2 = seq(0.1, 0.25, length = K),
-               tau2 = 0.35,
-               theta = seq(0.9, 1.5, length = K))
-starting <- list(sigma2 = seq(50, 100, length = K),
-                 theta = rep(0.5, K),
-                 sigf2 = 4,
-                 thf = 1.5, 
-                 tau2 = 1.5,
-                 beta = c(0, 0, 0))
-scen4 <- run.sims(4, 10, starting, propSD)
-cat("Scenario 4: \n")
-cat("Avg. Acceptance: \n")
-scen4$acceptance
-cat("Sigma2: \n")
-scen4$sigma2
-cat("Theta: \n")
-scen4$theta
-cat("Sigma2_f: \n")
-scen4$sigf2
-cat("Theta_f: \n")
-scen4$thetaf
-cat("Beta: \n")
-scen4$beta
-cat("Tau2: \n")
-scen4$tau2
-
-##### SCENARIO 5 #####
-propSD <- list(sigf2 = 0.6,
-               thf = 1.5,
-               sigma2 = seq(0.2, 0.4, length = K),
-               tau2 = 0.35,
-               theta = seq(0.9, 1.5, length = K))
-starting <- list(sigma2 = seq(5, 10, length = K),
-                 theta = rep(0.5, K),
-                 sigf2 = 4,
-                 thf = 1.5, 
-                 tau2 = 0.1,
-                 beta = c(0, 0, 0))
-scen5 <- run.sims(5, 10, starting, propSD)
-cat("Scenario 5: \n")
-cat("Avg. Acceptance: \n")
-scen5$acceptance
-cat("Sigma2: \n")
-scen5$sigma2
-cat("Theta: \n")
-scen5$theta
-cat("Sigma2_f: \n")
-scen5$sigf2
-cat("Theta_f: \n")
-scen5$thetaf
-cat("Beta: \n")
-scen5$beta
-cat("Tau2: \n")
-scen5$tau2
-
-##### SCENARIO 6 #####
-propSD <- list(sigf2 = 0.6,
-               thf = 0.7,
-               sigma2 = seq(0.1, 0.2, length = K),
-               tau2 = 0.35,
-               theta = seq(0.5, 0.9, length = K))
-starting <- list(sigma2 = seq(50, 100, length = K),
-                 theta = rep(5, K),
-                 sigf2 = 4,
-                 thf = 1.5, 
-                 tau2 = 0.1,
-                 beta = c(0, 0, 0))
-scen6 <- run.sims(1, 10, starting, propSD)
-cat("Scenario 6: \n")
-cat("Avg. Acceptance: \n")
-scen6$acceptance
-cat("Sigma2: \n")
-scen6$sigma2
-cat("Theta: \n")
-scen6$theta
-cat("Sigma2_f: \n")
-scen6$sigf2
-cat("Theta_f: \n")
-scen6$thetaf
-cat("Beta: \n")
-scen6$beta
-cat("Tau2: \n")
-scen6$tau2
-
-##### SCENARIO 7 #####
-propSD <- list(sigf2 = 0.6,
-               thf = 0.7,
-               sigma2 = seq(0.1, 0.2, length = K),
-               tau2 = 0.35,
-               theta = seq(0.5, 0.9, length = K))
-starting <- list(sigma2 = seq(50, 100, length = K),
-                 theta = rep(5, K),
-                 sigf2 = 4,
-                 thf = 1.5, 
-                 tau2 = 0.1,
-                 beta = c(0, 0, 0))
-scen7 <- run.sims(7, 10, starting, propSD)
-cat("Scenario 7: \n")
-cat("Avg. Acceptance: \n")
-scen7$acceptance
-cat("Sigma2: \n")
-scen7$sigma2
-cat("Theta: \n")
-scen7$theta
-cat("Sigma2_f: \n")
-scen7$sigf2
-cat("Theta_f: \n")
-scen7$thetaf
-cat("Beta: \n")
-scen7$beta
-cat("Tau2: \n")
-scen7$tau2
-
-##### SCENARIO 8 #####
-propSD <- list(sigf2 = 0.6,
-               thf = 1,
-               sigma2 = seq(0.1, 0.2, length = K),
-               tau2 = 0.35,
-               theta = seq(0.5, 0.9, length = K))
-starting <- list(sigma2 = seq(50, 100, length = K),
-                 theta = rep(5, K),
-                 sigf2 = 4,
-                 thf = 5, 
-                 tau2 = 0.1,
-                 beta = c(0, 0, 0))
-scen8 <- run.sims(8, 10, starting, propSD)
-cat("Scenario 8: \n")
-cat("Avg. Acceptance: \n")
-scen8$acceptance
-cat("Sigma2: \n")
-scen8$sigma2
-cat("Theta: \n")
-scen8$theta
-cat("Sigma2_f: \n")
-scen8$sigf2
-cat("Theta_f: \n")
-scen8$thetaf
-cat("Beta: \n")
-scen8$beta
-cat("Tau2: \n")
-scen8$tau2
-
-##### SCENARIO 9 #####
-propSD <- list(sigf2 = 0.6,
-               thf = 0.7,
-               sigma2 = seq(0.1, 0.2, length = K),
-               tau2 = 0.35,
-               theta = seq(0.5, 0.9, length = K))
-starting <- list(sigma2 = seq(50, 100, length = K),
-                 theta = rep(5, K),
-                 sigf2 = 10,
-                 thf = 1.5, 
-                 tau2 = 0.1,
-                 beta = c(0, 0, 0))
-scen9 <- run.sims(9, 10, starting, propSD)
-cat("Scenario 9: \n")
-cat("Avg. Acceptance: \n")
-scen9$acceptance
-cat("Sigma2: \n")
-scen9$sigma2
-cat("Theta: \n")
-scen9$theta
-cat("Sigma2_f: \n")
-scen9$sigf2
-cat("Theta_f: \n")
-scen9$thetaf
-cat("Beta: \n")
-scen9$beta
-cat("Tau2: \n")
-scen9$tau2
-
-##### SCENARIO 10 #####
-propSD <- list(sigf2 = 0.6,
-               thf = 0.7,
-               sigma2 = seq(0.1, 0.2, length = K),
-               tau2 = 0.35,
-               theta = seq(0.5, 0.9, length = K))
-starting <- list(sigma2 = seq(5, 10, length = K),
-                 theta = rep(5, K),
-                 sigf2 = 4,
-                 thf = 1.5, 
-                 tau2 = 0.1,
-                 beta = c(0, 0, 0))
-scen10 <- run.sims(10, 10, starting, propSD)
-cat("Scenario 10: \n")
-cat("Avg. Acceptance: \n")
-scen10$acceptance
-cat("Sigma2: \n")
-scen10$sigma2
-cat("Theta: \n")
-scen10$theta
-cat("Sigma2_f: \n")
-scen10$sigf2
-cat("Theta_f: \n")
-scen10$thetaf
-cat("Beta: \n")
-scen10$beta
-cat("Tau2: \n")
-scen10$tau2
-
-##### SCENARIO 11 #####
-propSD <- list(sigf2 = 0.6,
-               thf = 0.7,
-               sigma2 = seq(0.1, 0.2, length = K),
-               tau2 = 0.35,
-               theta = seq(0.5, 0.9, length = K))
-starting <- list(sigma2 = seq(50, 100, length = K),
-                 theta = rep(5, K),
-                 sigf2 = 4,
-                 thf = 1.5, 
-                 tau2 = 0.1,
-                 beta = c(0, 0, 0))
-scen11 <- run.sims(11, 10, starting, propSD)
-cat("Scenario 11: \n")
-cat("Avg. Acceptance: \n")
-scen11$acceptance
-cat("Sigma2: \n")
-scen11$sigma2
-cat("Theta: \n")
-scen11$theta
-cat("Sigma2_f: \n")
-scen11$sigf2
-cat("Theta_f: \n")
-scen11$thetaf
-cat("Beta: \n")
-scen11$beta
-cat("Tau2: \n")
-scen11$tau2
-
-
+nReps <- 10
+nCores <- 11
+cl <- makeCluster(nCores)
+registerDoParallel(cl)
+obj <- foreach(i = 1:nCores, .packages = "mvtnorm") %dopar% run.sims(i)
+stopCluster(cl)
