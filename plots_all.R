b49ea7668eec2c8439b19b79148fca7503989bfe
androsrj <androsrj@gmail.com>
2025-12-10 15:54:06 -0600

    nj plot and changed gp plots

--- plots_all.R
+++ plots_all.R
@@ -5,241 +5,154 @@ library(latex2exp)
 
 line.type <- 2
 line.width <- 4
-which.scens1 <- c(1, 4, 7)
-which.scens2 <- 11:13
-nScen <- length(which.scens1) + length(which.scens2)
+which.scens <- c(1:3, 7:9, 11:13)
+nScen <- length(which.scens)
 
 
 #################################
 ############# Beta0 #############
 #################################
 
-pdf("figures/new/beta0.pdf", width = 5.5, height = 12)
-layout.matrix <- matrix(c(1, 5, 
-                          2, 6,
-                          3, 7,
-                          4, 8), nrow = 4, ncol = 2, byrow = TRUE)
-layout(layout.matrix)
+pdf("figures/new/beta0.pdf", width = 5, height = 16)
+par(mar = c(2, 3, 3, 1), oma = c(0, 0, 3, 0), mfrow = c(nScen, 2))
 
-# Data for scens 1-10
-test <- readRDS(paste0("data/small/scen", 1, "/test.RDS"))
-nTest <- nrow(test$B)
-beta0.true <- test$U[ , 1] - test$U[ , 2]
-
-# True beta0, scens 1-10
-par(mar = c(3, 3, 5.5, 2) + 0.1)
-mba.data.true <- data.frame(test$U, beta0.true)
-mba.interp.true <- mba.surf(mba.data.true, no.X=100, no.Y=100, extend=TRUE)
-image(mba.interp.true$xyz.est, main = "(Scenarios 1 - 10)",
-      cex.main = 1.5, cex.lab = 1.5, cex.axis = 1.5, 
-      col = tim.colors(64))
-mtext(TeX("True surface of $\\beta_0$"), side = 3, line = 3.5)
-
-# Est. beta, scens 1, 4, 7
-par(mar = c(3, 3, 3, 2) + 0.1)
-for (i in which.scens1) {
+for (i in which.scens) {
+  
+  # Data for given scenario
+  test <- readRDS(paste0("data/small/scen", i, "/test.RDS"))
+  nTest <- nrow(test$B)
+  beta0.true <- test$B[ , 1]
+  
+  # True beta0 for given scenario
+  mba.data.true <- data.frame(test$U, beta0.true)
+  mba.interp.true <- mba.surf(mba.data.true, no.X=100, no.Y=100, extend=TRUE)
+  image(mba.interp.true$xyz.est, main = paste0("Scenario ", i, " "),
+        cex.main = 1.25, col = tim.colors(64))
+  
+  # Model results for given scenario
   path <- paste0("objects/small_scen", i, ".RDS") 
   results <- readRDS(path)[[1]]
   test <- readRDS(paste0("data/small/scen", i, "/test.RDS"))
   nTest <- nrow(test$B)
   beta0.means <- results$posteriorMeans$beta.test[1:nTest]
+  
+  # Estimated beta0 for given scenario
   mba.data <- data.frame(test$U, beta0.means)
   mba.interp <- mba.surf(mba.data, no.X=100, no.Y=100, extend=TRUE)
   image(mba.interp$xyz.est, main = paste0("Scenario ", i), 
-        col = tim.colors(64), cex.main = 1.5,
+        col = tim.colors(64), cex.main = 1.25,
         axes = FALSE)
+  
+  # Label each column of plots
+  mtext(TeX("True surface of $\\beta_0$"), 
+        side = 3, line = 1, outer = TRUE, 
+        at = 0.27, cex = 1.1)
+  mtext(TeX("Estimated surface of $\\beta_0$"), 
+        side = 3, line = 1, outer = TRUE, 
+        at = 0.77, cex = 1.1)
 }
-
-# Data for scens 11-13
-test <- readRDS(paste0("data/small/scen", 11, "/test.RDS"))
-nTest <- nrow(test$B)
-beta0.true <- test$B[ , 1]
-
-# True beta0, scens 11-13
-par(mar = c(3, 3, 5.5, 2) + 0.1)
-mba.data.true <- data.frame(test$U, beta0.true)
-mba.interp.true <- mba.surf(mba.data.true, no.X=100, no.Y=100, extend=TRUE)
-image(mba.interp.true$xyz.est, main = "(Scenarios 11 - 13)",
-      cex.main = 1.5, cex.lab = 1.5, cex.axis = 1.5, 
-      col = tim.colors(64))
-mtext(TeX("True surface of $\\beta_0$"), side = 3, line = 3.5)
-
-# Est. beta, scens 11-13
-par(mar = c(3, 3, 3, 2) + 0.1)
-for (i in which.scens2) {
-  path <- paste0("objects/small_scen", i, ".RDS") 
-  results <- readRDS(path)[[1]]
-  test <- readRDS(paste0("data/small/scen", i, "/test.RDS"))
-  nTest <- nrow(test$B)
-  beta0.means <- results$posteriorMeans$beta.test[1:nTest]
-  mba.data <- data.frame(test$U, beta0.means)
-  mba.interp <- mba.surf(mba.data, no.X=100, no.Y=100, extend=TRUE)
-  image(mba.interp$xyz.est, main = paste0("Scenario ", i), 
-        col = tim.colors(64), cex.main = 1.5,
-        axes = FALSE)
-}
-
 dev.off()
-
-
 
 
 #################################
 ############# Beta1 #############
 #################################
 
-pdf("figures/new/beta1.pdf", width = 5.5, height = 12)
-layout.matrix <- matrix(c(1, 5, 
-                          2, 6,
-                          3, 7,
-                          4, 8), nrow = 4, ncol = 2, byrow = TRUE)
-layout(layout.matrix)
+pdf("figures/new/beta1.pdf", width = 5, height = 16)
+par(mar = c(2, 3, 3, 1), oma = c(0, 0, 3, 0), mfrow = c(nScen, 2))
 
-# Data for scens 1-10
-test <- readRDS(paste0("data/small/scen", 1, "/test.RDS"))
-nTest <- nrow(test$B)
-beta1.true <- test$U[ , 1] + test$U[ , 2] - 100
-
-# True beta1, scens 1-10
-par(mar = c(3, 3, 5.5, 2) + 0.1)
-mba.data.true <- data.frame(test$U, beta1.true)
-mba.interp.true <- mba.surf(mba.data.true, no.X=100, no.Y=100, extend=TRUE)
-image(mba.interp.true$xyz.est, main = "(Scenarios 1 - 10)",
-      cex.main = 1.5, cex.lab = 1.5, cex.axis = 1.5, 
-      col = tim.colors(64))
-mtext(TeX("True surface of $\\beta_1$"), side = 3, line = 3.5)
-
-# Est. beta, scens 1, 4, 7
-par(mar = c(3, 3, 3, 2) + 0.1)
-for (i in which.scens1) {
+for (i in which.scens) {
+  
+  # Data for given scenario
+  test <- readRDS(paste0("data/small/scen", i, "/test.RDS"))
+  nTest <- nrow(test$B)
+  beta1.true <- test$B[ , 2]
+  
+  # True beta1 for given scenario
+  mba.data.true <- data.frame(test$U, beta1.true)
+  mba.interp.true <- mba.surf(mba.data.true, no.X=100, no.Y=100, extend=TRUE)
+  image(mba.interp.true$xyz.est, main = paste0("Scenario ", i, " "),
+        cex.main = 1.25, col = tim.colors(64))
+  
+  # Model results for given scenario
   path <- paste0("objects/small_scen", i, ".RDS") 
   results <- readRDS(path)[[1]]
   test <- readRDS(paste0("data/small/scen", i, "/test.RDS"))
   nTest <- nrow(test$B)
   beta1.means <- results$posteriorMeans$beta.test[(nTest+1):(2*nTest)]
+  
+  # Estimated beta1 for given scenario
   mba.data <- data.frame(test$U, beta1.means)
   mba.interp <- mba.surf(mba.data, no.X=100, no.Y=100, extend=TRUE)
   image(mba.interp$xyz.est, main = paste0("Scenario ", i), 
-        col = tim.colors(64), cex.main = 1.5,
+        col = tim.colors(64), cex.main = 1.25,
         axes = FALSE)
+  
+  # Label each column of plots
+  mtext(TeX("True surface of $\\beta_1$"), 
+        side = 3, line = 1, outer = TRUE, 
+        at = 0.27, cex = 1.1)
+  mtext(TeX("Estimated surface of $\\beta_1$"), 
+        side = 3, line = 1, outer = TRUE, 
+        at = 0.77, cex = 1.1)
 }
-
-# Data for scens 11-13
-test <- readRDS(paste0("data/small/scen", 11, "/test.RDS"))
-nTest <- nrow(test$B)
-beta1.true <- test$B[ , 2]
-
-# True beta1, scens 11-13
-par(mar = c(3, 3, 5.5, 2) + 0.1)
-mba.data.true <- data.frame(test$U, beta1.true)
-mba.interp.true <- mba.surf(mba.data.true, no.X=100, no.Y=100, extend=TRUE)
-image(mba.interp.true$xyz.est, main = "(Scenarios 11 - 13)",
-      cex.main = 1.5, cex.lab = 1.5, cex.axis = 1.5, 
-      col = tim.colors(64))
-mtext(TeX("True surface of $\\beta_1$"), side = 3, line = 3.5)
-
-# Est. beta, scens 11-13
-par(mar = c(3, 3, 3, 2) + 0.1)
-for (i in which.scens2) {
-  path <- paste0("objects/small_scen", i, ".RDS") 
-  results <- readRDS(path)[[1]]
-  test <- readRDS(paste0("data/small/scen", i, "/test.RDS"))
-  nTest <- nrow(test$B)
-  beta1.means <- results$posteriorMeans$beta.test[(nTest+1):(2*nTest)]
-  mba.data <- data.frame(test$U, beta1.means)
-  mba.interp <- mba.surf(mba.data, no.X=100, no.Y=100, extend=TRUE)
-  image(mba.interp$xyz.est, main = paste0("Scenario ", i), 
-        col = tim.colors(64), cex.main = 1.5,
-        axes = FALSE)
-}
-
 dev.off()
-
-
-
 
 
 #################################
 ############# Beta2 #############
 #################################
 
-pdf("figures/new/beta2.pdf", width = 5.5, height = 12)
-layout.matrix <- matrix(c(1, 5, 
-                          2, 6,
-                          3, 7,
-                          4, 8), nrow = 4, ncol = 2, byrow = TRUE)
-layout(layout.matrix)
+pdf("figures/new/beta2.pdf", width = 5, height = 16)
+par(mar = c(2, 3, 3, 1), oma = c(0, 0, 3, 0), mfrow = c(nScen, 2))
 
-# Data for scens 1-10
-test <- readRDS(paste0("data/small/scen", 1, "/test.RDS"))
-nTest <- nrow(test$B)
-beta2.true <- 2 * test$U[ , 1] - test$U[ , 2] - 50
-
-# True beta2, scens 1-10
-par(mar = c(3, 3, 5.5, 2) + 0.1)
-mba.data.true <- data.frame(test$U, beta2.true)
-mba.interp.true <- mba.surf(mba.data.true, no.X=100, no.Y=100, extend=TRUE)
-image(mba.interp.true$xyz.est, main = "(Scenarios 1 - 10)",
-      cex.main = 1.5, cex.lab = 1.5, cex.axis = 1.5, 
-      col = tim.colors(64))
-mtext(TeX("True surface of $\\beta_2$"), side = 3, line = 3.5)
-
-# Est. beta, scens 1, 4, 7
-par(mar = c(3, 3, 3, 2) + 0.1)
-for (i in which.scens1) {
+for (i in which.scens) {
+  
+  # Data for given scenario
+  test <- readRDS(paste0("data/small/scen", i, "/test.RDS"))
+  nTest <- nrow(test$B)
+  beta2.true <- test$B[ , 3]
+  
+  # True beta2 for given scenario
+  mba.data.true <- data.frame(test$U, beta2.true)
+  mba.interp.true <- mba.surf(mba.data.true, no.X=100, no.Y=100, extend=TRUE)
+  image(mba.interp.true$xyz.est, main = paste0("Scenario ", i, " "),
+        cex.main = 1.25, col = tim.colors(64))
+  
+  # Model results for given scenario
   path <- paste0("objects/small_scen", i, ".RDS") 
   results <- readRDS(path)[[1]]
   test <- readRDS(paste0("data/small/scen", i, "/test.RDS"))
   nTest <- nrow(test$B)
   beta2.means <- results$posteriorMeans$beta.test[(2*nTest+1):(3*nTest)]
+  
+  # Estimated beta2 for given scenario
   mba.data <- data.frame(test$U, beta2.means)
   mba.interp <- mba.surf(mba.data, no.X=100, no.Y=100, extend=TRUE)
   image(mba.interp$xyz.est, main = paste0("Scenario ", i), 
-        col = tim.colors(64), cex.main = 1.5,
+        col = tim.colors(64), cex.main = 1.25,
         axes = FALSE)
+  
+  # Label each column of plots
+  mtext(TeX("True surface of $\\beta_2$"), 
+        side = 3, line = 1, outer = TRUE, 
+        at = 0.27, cex = 1.1)
+  mtext(TeX("Estimated surface of $\\beta_2$"), 
+        side = 3, line = 1, outer = TRUE, 
+        at = 0.77, cex = 1.1)
 }
-
-# Data for scens 11-13
-test <- readRDS(paste0("data/small/scen", 11, "/test.RDS"))
-nTest <- nrow(test$B)
-beta2.true <- test$B[ , 3]
-
-# True beta0, scens 11-13
-par(mar = c(3, 3, 5.5, 2) + 0.1)
-mba.data.true <- data.frame(test$U, beta2.true)
-mba.interp.true <- mba.surf(mba.data.true, no.X=100, no.Y=100, extend=TRUE)
-image(mba.interp.true$xyz.est, main = "(Scenarios 11 - 13)",
-      cex.main = 1.5, cex.lab = 1.5, cex.axis = 1.5, 
-      col = tim.colors(64))
-mtext(TeX("True surface of $\\beta_2$"), side = 3, line = 3.5)
-
-# Est. beta, scens 11-13
-par(mar = c(3, 3, 3, 2) + 0.1)
-for (i in which.scens2) {
-  path <- paste0("objects/small_scen", i, ".RDS") 
-  results <- readRDS(path)[[1]]
-  test <- readRDS(paste0("data/small/scen", i, "/test.RDS"))
-  nTest <- nrow(test$B)
-  beta2.means <- results$posteriorMeans$beta.test[(2*nTest+1):(3*nTest)]
-  mba.data <- data.frame(test$U, beta2.means)
-  mba.interp <- mba.surf(mba.data, no.X=100, no.Y=100, extend=TRUE)
-  image(mba.interp$xyz.est, main = paste0("Scenario ", i), 
-        col = tim.colors(64), cex.main = 1.5,
-        axes = FALSE)
-}
-
 dev.off()
 
 
 #### Posterior distributions of tau2 for each scenario
 # Density plots for tau2
-pdf("figures/new/tau2.pdf", width = 10, height = 5)
-par(mfrow = c(2, 3), mar = c(4, 3, 3, 2) + 0.1, oma = c(0, 0, 4, 0))
-for (i in c(which.scens1, which.scens2)) {
+pdf("figures/new/tau2.pdf", width = 10, height = 8)
+par(mfrow = c(3, 3), mar = c(4, 3, 3, 2) + 0.1, oma = c(0, 0, 4, 0))
+for (i in which.scens) {
   path <- paste0("objects/small_scen", i, ".RDS") 
   results <- readRDS(path)[[1]]
   tau2.samples <- results$paramSamples$tau2
-  if (i == 4 | i == 12) {
+  if (i == 4 | i == 10 | i == 12) {
     true_tau2 <- 2
   } else {
     true_tau2 = 0.2
