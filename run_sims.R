19381bad4f0a8a373157d3eb686a7f64b8abaaca
Jacob Andros <androsrj@gmail.com>
2025-12-16 18:58:35 -0600

    re ran all plots

--- run_sims.R
+++ run_sims.R
@@ -21,7 +21,7 @@ run.mcmc <- function(rep) {
   results <- mcmc(X = X, Z = Z, Y = Y, D = D, K = K,
                   starting = starting,
                   propSD = propSD,
-                  nIter = 2000, nBurn = 1000, nThin=2,
+                  nIter = 5000, nBurn = 3000, nThin=2,
                   model = "full_gp")
   return(results)
 }
